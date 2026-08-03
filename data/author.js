adca7863430652a31266251223f5b98a125fc41d
Esimit Karlgusta <annoh.karlgusta@gmail.com>
2026-08-03 17:18:08 +0300

    Update profile pic EEAT

--- data/author.js
+++ data/author.js
@@ -4,7 +4,7 @@ export const author = {
   jobTitle: "Full-stack developer & indie maker",
   bio: "Karl Esi is a full-stack developer and indie maker based in Nanyuki, Kenya. He builds production SaaS products and developer tools that help founders ship faster. Creator of SassyPack and multiple live SaaS applications.",
   shortBio: "Full-stack developer & indie maker. Builds production SaaS products and starter kits that help developers ship faster.",
-  image: "/images/profile-pic.png",
+  image: "/images/profile-pic.jpg",
   imageAlt: "Karl Esi, creator of SassyPack",
   location: "Nanyuki, Kenya",
   email: "esimitkarlgusta@gmail.com",
